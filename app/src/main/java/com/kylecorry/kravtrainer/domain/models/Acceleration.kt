5071dfc5ac52027587e13c0ffb13632b0dd36de1
Kyle Corry <kylecorry31@gmail.com>
2020-03-23 15:29:37 -0400

    Remove unused acceleration code

--- app/src/main/java/com/kylecorry/kravtrainer/domain/models/Acceleration.kt
+++ app/src/main/java/com/kylecorry/kravtrainer/domain/models/Acceleration.kt
@@ -5,15 +5,8 @@ import kotlin.math.sqrt
 
 data class Acceleration(val x: Float, val y: Float, val z: Float){
 
-    fun distanceTo(other: Acceleration): Float {
-        return sqrt((x - other.x).pow(2) + (y - other.y).pow(2) + (z - other.z).pow(2))
-    }
-
     fun magnitude(): Float {
         return sqrt(x.pow(2) + y.pow(2) + z.pow(2))
     }
 
-    companion object {
-        val zero = Acceleration(0f, 0f, 0f)
-    }
 }
